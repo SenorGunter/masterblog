6620f5dc02d6d6df8191decb8e7ceb5cc8312d22
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2024-12-05 22:47:06 +0100

    Change in loading data / html and Css changes

--- app.py
+++ app.py
@@ -5,23 +5,29 @@ import uuid
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+def load_data():
     with open("blog_data.json", "r") as fileobj:
-        blog_posts = json.load(fileobj)
-        for blog in blog_posts:
-            print(blog)
-    if request.method == 'POST':
-        return redirect(url_for('add'))
-    return render_template("index.html", posts=blog_posts)
+        data = json.load(fileobj)
+    return data
 
 
 def generate_uid():
     return str(uuid.uuid4())
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """ Home site """
+    blog_posts = load_data()
+
+    if request.method == 'POST':
+        return redirect(url_for('add'))
+    return render_template("index.html", posts=blog_posts)
+
+
 @app.route('/add', methods=["GET", "POST"])
 def add():
+    """ Function to add a new blog entry """
     render_template('add.html')
     if request.method == 'POST':
         blog_author = request.form.get("author")
@@ -30,9 +36,7 @@ def add():
 
         unique_id = generate_uid()
 
-        with open("blog_data.json", "r") as fileobj:
-            data = json.load(fileobj)
-
+        data = load_data()
 
         new_data = {"id": unique_id, "author": blog_author, "title": blog_title, "content": blog_content}
 
@@ -46,10 +50,9 @@ def add():
 
 @app.route("/delete/<post_id>", methods=["GET", "POST"])
 def delete(post_id):
+    """ Delete a post and reload the site with updated data"""
     post_to_delete = None
-
-    with open("blog_data.json", "r") as fileobj:
-        data = json.load(fileobj)
+    data = load_data()
 
     for post in data:
         if post_id == post.get("id"):
@@ -58,7 +61,6 @@ def delete(post_id):
 
     if post_to_delete:
         data.remove(post_to_delete)
-
         with open("blog_data.json", "w") as fileobj:
             json.dump(data, fileobj)
         print("Post deleted")
