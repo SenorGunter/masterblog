483ea0be45701eb2d858af65da2ecde8d450640a
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2024-12-06 02:34:20 +0100

    Added update function / other small changes

--- app.py
+++ app.py
@@ -21,14 +21,21 @@ def index():
     blog_posts = load_data()
 
     if request.method == 'POST':
-        return redirect(url_for('add'))
+        action = request.form.get("action")
+        post_id = request.form.get("post_id")
+
+        if action == "add":
+            return redirect(url_for("add"))
+        elif action == "update":
+            return redirect(url_for("update", post_id=post_id))
+
     return render_template("index.html", posts=blog_posts)
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
     """ Function to add a new blog entry """
-    render_template('add.html')
+
     if request.method == 'POST':
         blog_author = request.form.get("author")
         blog_title = request.form.get("title")
@@ -68,6 +75,38 @@ def delete(post_id):
         print("404: ID not found")
 
     return render_template("index.html", posts=data)
+
+
+@app.route("/update/<post_id>", methods=["GET", "POST"])
+def update(post_id):
+    data = load_data()
+    post_to_update = None
+
+    for post in data:
+        if post_id == post.get("id"):
+            post_to_update = post
+            break
+
+    print(post_to_update)
+    if post_to_update is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        updated_author = request.form.get("author")
+        updated_title = request.form.get("title")
+        updated_content = request.form.get("content")
+
+        post_to_update['author'] = updated_author
+        post_to_update['title'] = updated_title
+        post_to_update['content'] = updated_content
+
+        with open("blog_data.json", "w") as fileobj:
+            json.dump(data, fileobj)
+        return render_template("index.html", posts=data)
+
+    return render_template('update.html', post=post_to_update)
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
